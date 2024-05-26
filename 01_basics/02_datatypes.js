af267aab3847e0568a899e912e34daca6eaab787
Mrr-Sandy <saurabhmasiyal345@gmail.com>
2024-05-26 14:12:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+let isboolean = false;
+let state;
+
+
+// data types 
+// number => 2 t0 power 53
+// string =>  ""
+// boolean => true/false
+//null =. standalone value
+//symbol
+
+
+//object
+
+console.log(typeof undefined);  //undefined
+
+console.log(typeof null);  //undefined
