27824dbf1f6343bd18914056af96818c4afac4a6
Mrr-Sandy <saurabhmasiyal345@gmail.com>
2024-05-26 14:36:03 +0000

    conversion Operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score  = null;
+
+console.log(typeof(score));
+console.log(typeof Number(score));// conversion in number
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber);
+console.log(valueNumber)
+
+
+//"33" => 33
+//"33abc" => Nan
+//true=> 1; false =>0
